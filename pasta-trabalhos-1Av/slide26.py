0db31e75d75b2abfe8ac67ba60559cc432512e77
Raiane <raianw200410@outlook.com>
2023-05-24 09:54:05 -0300

    memory

--- pasta-trabalhos-1Av/slide26.py
+++ pasta-trabalhos-1Av/slide26.py
@@ -1,11 +1,11 @@
-import math
+# import math
 
-pergunta = int(input("Escreva um valor para níveis de quantização que seja resultado de uma potência de 2: "))
-bits = int(math.log2(pergunta))
+# pergunta = int(input("Escreva um valor para níveis de quantização que seja resultado de uma potência de 2: "))
+# bits = int(math.log2(pergunta))
 
-if 2**bits == pergunta:
-  for i in range(2**bits):
-    print(bin(i)[2:].zfill(bits), '=', i)
-  print(f"Cada um terá {bits} Bits e haverão {2**bits} possíveis combinações")
-else:
-  print("Esse número não é o resultado de uma potência de 2!")
+# if 2**bits == pergunta:
+#   for i in range(2**bits):
+#     print(bin(i)[2:].zfill(bits), '=', i)
+#   print(f"Cada um terá {bits} Bits e haverão {2**bits} possíveis combinações")
+# else:
+#   print("Esse número não é o resultado de uma potência de 2!")
